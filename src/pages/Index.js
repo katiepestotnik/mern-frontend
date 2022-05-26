ae075ebd943dec2efa9ec3ff286d23b89f678fb0
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-05-25 22:35:15 -0600

    finish index

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,12 +1,34 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Index = ({ people, createPeople }) => {
+    //state to hodl form data
+    const [form, setForm] = useState({
+        name: '',
+        image: '',
+        title:''
+    })
+    //handleChange
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]:e.target.value})
+    }
+    //handleSubmit
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        createPeople(form)
+        setForm({
+                name: '',
+                image: '',
+                title:''
+        })
+    }
+
     const loaded = () => {
         return people.map((person => (
             <div
                 key={person._id}
                 className="person">
-                <Link to={`/people/${person.id}`}>
+                <Link to={`/people/${person._id}`}>
                     <h1>{person.name}</h1>
                 </Link>
                 <img src={person.image} alt={person.name} />
@@ -18,7 +40,35 @@ const Index = ({ people, createPeople }) => {
     const loading = () => {
         return(<h1>Loading</h1>)
     }
-    return people ? loaded() : loading()
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder='NAME'
+                    value={form.name}
+                    onChange={handleChange}
+                />
+                <input
+                    type="url"
+                    name="image"
+                    placeholder='IMAGE URL'
+                    value={form.image}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    name="title"
+                    placeholder='TITLE'
+                    value={form.title}
+                    onChange={handleChange}
+                />
+                <button>ENTER</button>
+            </form>
+            {people ? loaded() : loading()}
+        </section>
+    )
     
 
 }
